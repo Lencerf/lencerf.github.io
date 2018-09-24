ccaee5836247c2129de99c2faeb88af3aed08b9e
Lencerf <changyuan.lv@gmail.com>
2018-09-23 23:38:58 -0400

    update

--- vnet_pac.js
+++ vnet_pac.js
@@ -2,7 +2,17 @@ function FindProxyForURL(url, host){
     var TOR = "SOCKS5 127.0.0.1:9050; SOCKS 127.0.0.1:9050";
 	var SSR = "SOCKS5 127.0.0.1:1082; SOCKS 127.0.0.1:1082";
 	var SS1083 = "SOCKS5 127.0.0.1:1083; SOCKS 127.0.0.1:1083";
-	var P = '013:eno.tenv.5-enilsh YXORP';
+    var BLACKHOLE = "PROXY 127.0.0.1:18080";
+
+
+    var black = [
+        "e-hentai.org",
+        "bilibili.com",
+        "mikanani.me",
+        "twitter.com",
+        "facebook.com",
+        "weibo.com"
+    ];
 
     var domains = [
         "dl.lux.bookfi.net",
@@ -18,6 +28,16 @@ function FindProxyForURL(url, host){
 		"bangumi.bilibili.com",
         "music.163.com"
     ];
+
+    if(myIpAddress().startsWith("10.186") || myIpAddress().startsWith("192.168.1.233")) { // at school
+        if (weekdayRange("MON", "FRI") && timeRange(9, 18)) { // work_time
+            for (var i = black.length - 1; i >= 0; i--) {
+                if (dnsDomainIs(host, black[i])) {
+                    return BLACKHOLE;
+                }
+            }  
+        }
+    }
     
     if(dnsDomainIs(host,"schwab.com") && myIpAddress().startsWith("10.186")) {
         return SSR;
