b0be0875a7cee71c0581fdc49aa0af0bed54d567
Lencerf <changyuan.lv@Gmail.com>
2019-02-22 00:50:53 -0500

    Update vnet_pac_m.js

--- vnet_pac_m.js
+++ vnet_pac_m.js
@@ -1,32 +1,18 @@
-function FindProxyForURL(url, host){
-	var P = '08001:4.98.032.53 YXORP';
-	
-	var bili = [
-		"bangumi.bilibili.com",
-		"music.163.com",
-		"music.126.net",
-		"ip.ws.126.net",
-		"music.httpdns.c.163.com",
-		"m10.music.126.net"
-	];
+var V2Ray = "SOCKS5 35.232.35.51:1081; SOCKS 35.232.35.51:1081; DIRECT;";
 
-    var ips = [
-        "223.252.199.66",
-        "103.65.41.126"
-    ]
+var domains = [
+	// "api.bilibili.com",
+	// "bangumi.bilibili.com",
+	// "tieba.baidu.com",
+	"music.163.com"
+];
 
-	for (var i = bili.length - 1; i >= 0; i--) {
-    	if (dnsDomainIs(host, bili[i])) {
-    		return P.split("").reverse().join("");
+
+function FindProxyForURL(url, host) {
+    for (var i = domains.length - 1; i >= 0; i--) {
+    	if (dnsDomainIs(host, domains[i])) {
+            return V2Ray;
     	}
 	}
-   /* 
-    var ip = dnsResolve(host);
-    for (var i = ips.length - 1; i >= 0; i--) {
-        if (ip == ips[i]) {
-            return P.split("").reverse().join("");
-        }
-    }
-*/
-	return 'DIRECT';
+    return "DIRECT";
 }
